31af0fbeb1ce83a80b0b9a2f648b0373c894bc54
chicodog530 <chicodog530@yahoo.com>
2026-08-16 15:53:42 -0500

    Remove unused secrets.h from repository

--- secrets.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-// ###########################################################################
-// USER-CHANGEABLE PRIVATE WI-FI SETTINGS
-// (These are no longer used since the transceiver creates its own Hotspot!)
-// ###########################################################################
-#define WIFI_SSID "Adams-WIFI"
-#define WIFI_PASSWORD "larren74"
